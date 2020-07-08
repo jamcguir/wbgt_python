de1465bc946a148ca05ec04613de9c2566ab1047
root <jamcguir@ncsu.edu>
2020-07-08 17:18:32 -0400

    Aside from testing limit of t=2, make other limits based on file dim than a defined expected value

--- utilities.py
+++ utilities.py
@@ -110,6 +110,8 @@ def RTMA_import(filename):
         data_d[v] = np.squeeze(rootgrp[v][:])
         if(data_d[v].ndim == 3):
           data_d[v] = np.swapaxes(data_d[v], 0, 2)
+	  # JUST DO 2 HOURS FOR NOW FOR TESTING
+          data_d[v] = data_d[v][:,:,:2] 
         data_d[v].fill_value = np.nan
         unit_d[v] = rootgrp[v].getncattr("units")
         if '_FillValue' in rootgrp[v].ncattrs():
@@ -131,9 +133,10 @@ def NDFD2_import(filename):
         #data_d[v] = rootgrp[v][:]
         if(data_d[v].ndim == 3):
           data_d[v] = np.swapaxes(data_d[v], 0, 2)
+          data_d[v] = data_d[v][:,:,:46] 
+	  # JUST DO 2 HOURS FOR NOW FOR TESTING
+          data_d[v] = data_d[v][:,:,:2] 
         data_d[v].fill_value = np.nan
-        #data_d[v] = data_d[v][:,:,:46] 
-        #data_d[v] = data_d[v][:46,:,:] #No swap axes, so t,y,x
         if '_FillValue' in rootgrp[v].ncattrs():
           fill_d[v] = rootgrp[v].getncattr("_FillValue")
         else:
@@ -154,6 +157,8 @@ def small_import(filename):
         data_d[v] = rootgrp[v][:]
         if(data_d[v].ndim == 3):
           data_d[v] = np.swapaxes(data_d[v], 0, 2)
+	  # JUST DO 2 HOURS FOR NOW FOR TESTING
+          data_d[v] = data_d[v][:,:,:2] 
     return vars_d, data_d
 
 #%%% Bias Functions
@@ -174,7 +179,8 @@ def RTMA_bias(data_RTMA, z):
             "wind_bias": np.array([0.12,0.14,0.14,0.15,0.2,0.26,0.26,0.27,0.27,0.23,0.22,0.25,0.21,0.17,0.09,0.07,-0.01,-0.15,-0.02,0.09,0.1,0.11,0.1,0.12,0.12]),
             "srad_bias": np.array([0,0,0,0.13,21.51,90.23,82.56,35.49,-24.2,-87.89,-160.95,-229.6,-272.25,-287.4,-271.61,-221.73,-154.68,-68.81,-1.86,0.08,0,0,0,0,0])
             }
-    for i in range(0, 25):  
+    ntime = data_RTMA["TMP_2maboveground"].shape[2]
+    for i in range(0, ntime):  
         data_RTMA["TMP_2maboveground"][:, :, i] = data_RTMA["TMP_2maboveground"][:, :, i] + bias_table["temp_bias"][i]
         data_RTMA["DPT_2maboveground"][:, :, i] = data_RTMA["DPT_2maboveground"][:, :, i] + bias_table["dew_bias"][i]
         data_RTMA["WIND_10maboveground"][:, :, i] = data_RTMA["WIND_10maboveground"][:, :, i] + bias_table["wind_bias"][i]
@@ -188,7 +194,8 @@ def NDFD_bias(data_NDFD, z):
         "wind_bias": np.array([0.5,0.7,0.6,0.6,0.6,0.5,0.4,0.5,0.4,0.4,0.4,0.4,0,0.2,0.3,0.3,0.5,0.5,0.2,0.6,0.7,0.6,0.8,0.9]),
         "srad_bias": np.array([0.2,-0.2,0,0,0,0,0,0,0,0,-8,-64.1,-103.1,-78.9,-34.1,22.9,76.8,133,182.1,189.5,179.9,139.5,91.5,29.5])
         }
-    for i in range(0, 25):  
+    ntime = data_NDFD["TMP_2maboveground"].shape[2]
+    for i in range(0, ntime):  
         data_NDFD["TMP_2maboveground"][:, :, i] = data_NDFD["TMP_2maboveground"][:, :, i] + bias_table["temp_bias"][i]
         data_NDFD["DPT_2maboveground"][:, :, i] = data_NDFD["DPT_2maboveground"][:, :, i] + bias_table["dew_bias"][i]
         data_NDFD["WIND_10maboveground"][:, :, i] = data_NDFD["WIND_10maboveground"][:, :, i] + bias_table["wind_bias"][i]
@@ -202,7 +209,8 @@ def NBM_bias(data_NBM, z):
         "wind_bias": np.array([0,-0.1,-0.1,-0.1,-0.1,-0.1,-0.1,-0.1,-0.1,-0.1,-0.1,-0.2,-0.3,-0.3,-0.2,-0.2,-0.2,-0.2,-0.3,-0.2,-0.1,-0.1,0,0.1]),
         "srad_bias": np.array([0.2,-0.2,0,0,0,0,0,0,0,0,-8,-64.1,-101.7,-74.1,-23.1,39.7,99,155.5,197.2,208,193.6,150.5,95.8,30.5])
         }
-    for i in range(0, 25):  
+    ntime = data_NBM["TMP_2maboveground"].shape[2]
+    for i in range(0, ntime):  
         data_NBM["TMP_2maboveground"][:, :, i] = data_NBM["TMP_2maboveground"][:, :, i] + bias_table["temp_bias"][i]
         data_NBM["DPT_2maboveground"][:, :, i] = data_NBM["DPT_2maboveground"][:, :, i] + bias_table["dew_bias"][i]
         data_NBM["WIND_10maboveground"][:, :, i] = data_NBM["WIND_10maboveground"][:, :, i] + bias_table["wind_bias"][i]
@@ -219,7 +227,9 @@ def srad_bias(data_srad, z=12, dataset="srad_bias_RTMA"):
         bias_table = {
             "srad_bias_RTMA": np.array([0,0,0,0.13,21.51,90.23,82.56,35.49,-24.2,-87.89,-160.95,-229.6,-272.25,-287.4,-271.61,-221.73,-154.68,-68.81,-1.86,0.08,0,0,0,0,0])
             }
-    for i in range(0, 25):  
+
+    ntime = data_srad.shape[2]
+    for i in range(0, ntime):  
         data_srad[:, :, i] = data_srad[:, :, i] + bias_table[dataset][i]
     return data_srad
 
